82c12d8ced8c29761ff2bcbf390da98a819e8aad
shuuuuuny <shuuuuuny+git@gmail.com>
2022-01-22 13:30:34 +0900

    Update brew-npm v0.3.0

--- brew-npm.rb
+++ brew-npm.rb
@@ -1,8 +1,8 @@
 class BrewNpm < Formula
   desc "Install npm packages as Homebrew formula"
   homepage "https://github.com/shuuuuun/brew-npm"
-  url "https://github.com/shuuuuun/brew-npm/archive/v0.2.0.tar.gz"
-  sha256 "d9b965b53e743f9055ac5658e0c1ba9fa6f5dd4ddca8cf9fcb31de6ac6524799"
+  url "https://github.com/shuuuuun/brew-npm/archive/v0.3.0.tar.gz"
+  sha256 "cda3089ab7fcce8be41c1f40dbf298bf74e8bb36fe40065ed05981bbca1a78c1"
   license "MIT"
   head "https://github.com/shuuuuun/brew-npm.git"
 
